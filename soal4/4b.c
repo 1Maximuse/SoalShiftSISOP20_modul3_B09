56996bb80f0f128c5799ba9415735a37d87752af
rozakcloud <61219666+rozakcloud@users.noreply.github.com>
2020-04-02 21:43:28 +0700

    Update 4b.c

--- soal4/4b.c
+++ soal4/4b.c
@@ -3,51 +3,21 @@
 #include <sys/shm.h>
 #include <unistd.h>
 #include <pthread.h>
-#define MAX 500
 
-int multiply(int x, int res[], int res_size);
-void factorial(int n) 
-{ 
-	int res[MAX]; 
-
-	res[0] = 1; 
-	int res_size = 1; 
-
-	for (int x=2; x<=n; x++) {
-                res_size = multiply(x, res, res_size);
-        }
-	
-	for (int i=res_size-1; i>=0; i--){
-               printf("%d",res[i]);
-        }  
-}
-		
-int multiply(int x, int res[], int res_size) 
-{ 
-	int carry = 0;
-	for (int i=0; i<res_size; i++) 
-	{ 
-		int prod = res[i] * x + carry; 
-
-		res[i] = prod % 10; 
-
-		carry = prod/10;	 
-	} 
-
-	while (carry) 
-{ 
-		res[res_size] = carry%10; 
-		carry = carry/10; 
-		res_size++; 
-	} 
-	return res_size; 
-} 
-
-void *fac(int angka)
+void jumlah(int n)
 {
-        factorial(angka);
+	int sum=0;
+	for(n;n>0;n--)
+	{
+		sum+=n;
+	}
+	printf("%d",sum);
+        // return sum;
 }
-
+void *jum(int angka)
+{
+        jumlah(angka);
+}
 void main()
 {
         key_t key = 1234;
@@ -60,10 +30,10 @@ void main()
         pthread_t tid[20];
         int idx;
         for (int i = 0;i < 20;i++)
-	      {
+	{
                 arr[i]=*value;
                 angka=arr[i];
-                pthread_create(&tid[idx],NULL, &fac, (void*)angka);
+                pthread_create(&tid[idx],NULL, &jum, (void*)angka);
                 idx++;
                 sleep(2);
                 if(i%5==4 && i>0)
