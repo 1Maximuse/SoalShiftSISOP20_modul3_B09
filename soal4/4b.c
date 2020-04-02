4172c153a1cb70dddaf7cea9b8860d0f94eaa5be
rozakcloud <61219666+rozakcloud@users.noreply.github.com>
2020-04-02 23:13:23 +0700

    Update 4b.c

--- soal4/4b.c
+++ soal4/4b.c
@@ -4,55 +4,27 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
-#define MAX 500
-		
-int multiply(int x, int res[], int res_size) 
-{ 
-	int carry = 0;
-	for (int i=0; i<res_size; i++) 
-	{ 
-		int prod = res[i] * x + carry; 
-		res[i] = prod % 10; 
-		carry = prod/10;	 
-	} 
-
-	while (carry) 
-	{
-		res[res_size] = carry%10; 
-		carry = carry/10; 
-		res_size++; 
-	} 
-	return res_size; 
-} 
-
-void factorial(int n, char* c) 
-{ 
-	int res[MAX];
-
-	res[0] = 1; 
-	int res_size = 1; 
-
-	for (int x=2; x<=n; x++) {
-		res_size = multiply(x, res, res_size);
-	}
-	
-	for (int i=res_size-1; i>=0; i--){
-		char cc[2];
-		sprintf(cc, "%d", res[i]);
-		strcat(c, cc);
-	}
-}
 
 typedef struct data {
 	int angka;
-	char hasil[1500];
+	int hasil;
 }data;
 
-void *fac(void* arg)
+int jumlah(int n) 
+{ 
+	int sum=0;
+	for(n;n>0;n--)
+	{
+		sum+=n;
+	}
+	return sum;
+}
+
+void *jumn(void* arg)
 {
 	data* d = (data*) arg;
-	memset(d->hasil, 0, sizeof(d->hasil));
-	factorial(d->angka, d->hasil);
+	//memset(d->hasil, 0, sizeof(d->hasil));
+        d->hasil=jumlah(d->angka);
 }
 void main()
 {
@@ -68,13 +40,13 @@ void main()
 		d[i].angka = value[i];
 		if (i % 5 == 0) printf("\n");
 		printf("%4d", value[i]);
-		pthread_create(&tid[i],NULL, &fac, (void*)&d[i]);
+		pthread_create(&tid[i],NULL, &jumn, (void*)&d[i]);
 	}
 	printf("\n");
 	for(int i=0; i< 20; i++)
 	{
 		if (i % 5 == 0) printf("\n");
-		printf("%20s", d[i].hasil);
+		printf("%d\t", d[i].hasil);
 		pthread_join(tid[i],NULL);
 	}
 	printf("\n");
