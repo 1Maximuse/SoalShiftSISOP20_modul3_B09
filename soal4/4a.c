95bd7b4bcc0c7b7b64e609500576ae497a4047cf
rozakcloud <61219666+rozakcloud@users.noreply.github.com>
2020-04-02 18:58:07 +0700

    Create 4a.c

---
+++ soal4/4a.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+int main()
+{
+    key_t key = 1234;
+    int *value;
+    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0644);
+    value = shmat(shmid, NULL, 0);
+ 
+  int m, n, p, q, c, d, k, sum = 0;
+  int first[4][2] = {{3, 1},
+                    {1, 2},
+                    {3, 3},
+                    {0, 2}};
+  int second[2][5] ={{1, 2, 0, 3, 2},
+                    {1, 2, 2, 3, 2}};
+  int multiply[4][5];
+
+    for (c = 0; c < 4; c++) {
+      for (d = 0; d < 5; d++) {
+        for (k = 0; k < 2; k++) {
+          sum = sum + first[c][k]*second[k][d];
+        }
+        multiply[c][d] = sum;
+        sum = 0;
+      }
+    }
+ 
+  for (c = 0; c < 4; c++) 
+	{
+    for (d = 0; d < 5; d++)
+	  {
+      *value = multiply[c][d];
+      sleep(2);
+      printf("%d\n",*value);
+        // printf("%d\t", multiply[c][d]);
+    }
+      // printf("\n");
+  }
+  shmdt(value);
+  shmctl(shmid, IPC_RMID, NULL);
+  return 0;
+}
